f818a0691bfd32553dbe402d0b50b69c0330e64f
Soum110 <145447583+Soum110@users.noreply.github.com>
2026-05-10 14:12:45 +0530

    Time: 35 ms (14.25%) | Memory: 19.5 MB (98.38%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,18 +1,24 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        #Creating the 2D array [[....][....]..[....]]
         arr = [['.' for _ in range(n)] for _ in range(n)]
+        #Result 
         res = []
+        #Recursive function to check for validity of each column for placing the 'Q'
         def f(col, arr):
-            if col == n:
+            #Boundary condition
+            if col == n:    #When column value exceeds the len(col) append the present array to result array by coping 
                 res.append([''.join(r) for r in arr])
                 return
-            
-            for row in range(n):
+            #Recursive calling
+            for row in range(n):    #Iterate through each row of each column and check validity to place 'Q'
                 if (isSafe(col, row, arr)):
                     arr[row][col] = 'Q'
                     f(col + 1, arr)
                     arr[row][col] = '.'
-        def isSafe(col, row, arr):
+        def isSafe(col, row, arr):  #Implementing the validity checker 
+
+            #Checks for upper diagonal for any other 'Q'
             tcol = col
             trow = row
             while (tcol >= 0 and trow >= 0):
@@ -21,6 +27,7 @@ class Solution:
                 trow -= 1
                 tcol -= 1
             
+            #Checks for the present row for any other 'Q'
             trow = row
             tcol = col
             while tcol >= 0:
@@ -28,6 +35,7 @@ class Solution:
                     return False
                 tcol -= 1
             
+            #Checks for lower diagonal for any other 'Q'
             trow = row
             tcol = col
             while (tcol >= 0 and trow < n):
@@ -35,7 +43,10 @@ class Solution:
                     return False
                 tcol -= 1
                 trow += 1
+            
+            #If not found return True
             return True
+        
         f(0, arr)
         return res
 
